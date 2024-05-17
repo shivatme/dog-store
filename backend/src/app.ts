62c53663f2a2d38b2f367c34a4c3af1ca86ee274
Shivam5278 <st527874@gmail.com>
2024-05-18 02:44:31 +0530

    Added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import rootRouter from "./routes/index";
 
@@ -8,6 +9,7 @@ const app: Express = express();
 
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.json());
+app.use(cors());
 app.use(
   bodyParser.urlencoded({
     extended: true,
